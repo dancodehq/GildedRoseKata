c2b5298a28cdacdcaf64ce5d28881b8a6720243b
Dan Harrison <harrison92d@gmail.com>
2024-08-07 18:10:44 +0100

    Replace if with respective branch

--- src/GildedRose/GildedRose.cs
+++ src/GildedRose/GildedRose.cs
@@ -73,40 +73,11 @@ namespace GildedRoseKata
                     }
                     else
                     {
-                        if (!false)
+                        if (item.Quality > 0)
                         {
-                            if (item.Quality > 0)
+                            if (item.Name != "Sulfuras, Hand of Ragnaros")
                             {
-                                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    item.Quality = item.Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-
-                                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                                {
-                                    if (item.SellIn < 11)
-                                    {
-                                        if (item.Quality < 50)
-                                        {
-                                            item.Quality = item.Quality + 1;
-                                        }
-                                    }
-
-                                    if (item.SellIn < 6)
-                                    {
-                                        if (item.Quality < 50)
-                                        {
-                                            item.Quality = item.Quality + 1;
-                                        }
-                                    }
-                                }
+                                item.Quality = item.Quality - 1;
                             }
                         }
 
