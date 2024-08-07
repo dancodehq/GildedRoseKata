4cc7676b948c7ef64121003c0419b0589b887d08
Dan Harrison <harrison92d@gmail.com>
2024-08-07 17:18:47 +0100

    Assert that aged brie quality improves by one each day when sell by is in the future

--- src/GildedRoseTests/GildedRoseTest.cs
+++ src/GildedRoseTests/GildedRoseTest.cs
@@ -42,5 +42,14 @@ namespace GildedRoseTests
             Assert.Equal(0, items[0].Quality);
         }
 
+        [Fact]
+        public void Aged_brie_quality_improves_by_one_each_day_when_sell_by_is_in_the_future()
+        {
+            List<Item> items = [new Item { Name = "Aged Brie", Quality = 30, SellIn = 5 }];
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(31, items[0].Quality);
+        }
+
     }
 }
