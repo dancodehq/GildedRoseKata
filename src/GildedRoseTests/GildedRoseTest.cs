9ea27ff95ff34f6a0d1c22836a9fce99ea26e137
Dan Harrison <harrison92d@gmail.com>
2024-08-07 17:10:28 +0100

    Assert that normal item quality degrades by two each day when sell by is exceeded

--- src/GildedRoseTests/GildedRoseTest.cs
+++ src/GildedRoseTests/GildedRoseTest.cs
@@ -14,5 +14,15 @@ namespace GildedRoseTests
             app.UpdateQuality();
             Assert.Equal(9, items[0].Quality);
         }
+        
+        [Fact]
+        public void Normal_item_quality_degrades_by_two_each_day_when_sell_by_is_exceeded()
+        {
+            List<Item> items = [new Item { Name = "Sword", Quality = 10, SellIn = 0 }];
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(8, items[0].Quality);
+        }
+
     }
 }
