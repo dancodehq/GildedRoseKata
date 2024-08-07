0dc020e805162e24c5de650748ce7e278e662aa5
Dan Harrison <harrison92d@gmail.com>
2024-08-07 18:00:53 +0100

    Replace if with respective branch

--- src/GildedRose/GildedRose.cs
+++ src/GildedRose/GildedRose.cs
@@ -17,38 +17,25 @@ namespace GildedRoseKata
                 var isAgedBrie = item.Name == "Aged Brie";
                 if (isAgedBrie)
                 {
-                    if (!true && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.Quality < 50)
                     {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
+                        item.Quality = item.Quality + 1;
 
-                            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (item.SellIn < 11)
                             {
-                                if (item.SellIn < 11)
+                                if (item.Quality < 50)
                                 {
-                                    if (item.Quality < 50)
-                                    {
-                                        item.Quality = item.Quality + 1;
-                                    }
+                                    item.Quality = item.Quality + 1;
                                 }
+                            }
 
-                                if (item.SellIn < 6)
+                            if (item.SellIn < 6)
+                            {
+                                if (item.Quality < 50)
                                 {
-                                    if (item.Quality < 50)
-                                    {
-                                        item.Quality = item.Quality + 1;
-                                    }
+                                    item.Quality = item.Quality + 1;
                                 }
                             }
                         }
