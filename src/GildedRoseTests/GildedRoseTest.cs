c8935ff079b7b8adc72a02b51879317f9a1c2d7b
Dan Harrison <harrison92d@gmail.com>
2024-08-07 17:08:32 +0100

    Assert that normal item quality degrades by one each day when sell by is in the future

--- src/GildedRoseTests/GildedRoseTest.cs
+++ src/GildedRoseTests/GildedRoseTest.cs
@@ -6,6 +6,13 @@ namespace GildedRoseTests
 {
     public class GildedRoseTest
     {
-        
+        [Fact]
+        public void Normal_item_quality_degrades_by_one_each_day_when_sell_by_is_in_the_future()
+        {
+            List<Item> items = [new Item { Name = "Sword", Quality = 10, SellIn = 5 }];
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(9, items[0].Quality);
+        }
     }
 }
