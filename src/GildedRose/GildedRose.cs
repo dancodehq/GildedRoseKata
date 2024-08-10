4a377e4c8230116896df700ad99934646e568228
Dan Harrison <harrison92d@gmail.com>
2024-08-10 10:41:20 +0100

    Duplicate UpdateNormal method into new NormalItemStrategy class

--- src/GildedRose/GildedRose.cs
+++ src/GildedRose/GildedRose.cs
@@ -32,6 +32,34 @@ namespace GildedRoseKata
             }
         }
 
+        class NormalItemStrategy
+        {
+            private readonly Item _item;
+
+            public NormalItemStrategy(Item item)
+            {
+                _item = item;
+            }
+
+            public void Update()
+            {
+                if (_item.Quality > 0)
+                {
+                    _item.Quality = _item.Quality - 1;
+                }
+
+                _item.SellIn = _item.SellIn - 1;
+
+                if (_item.SellIn < 0)
+                {
+                    if (_item.Quality > 0)
+                    {
+                        _item.Quality = _item.Quality - 1;
+                    }
+                }
+            }
+        }
+
         private void UpdateNormal(Item item)
         {
             if (item.Quality > 0)
