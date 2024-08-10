2c2351c23d060af8ce55c6379f707305752f26ff
Dan Harrison <harrison92d@gmail.com>
2024-08-10 15:33:10 +0100

    Add conjured item implementation

--- src/GildedRose/GildedRose.cs
+++ src/GildedRose/GildedRose.cs
@@ -26,6 +26,7 @@ namespace GildedRoseKata
                 "Aged Brie" => new AgedBrieStrategy(item),
                 "Backstage passes to a TAFKAL80ETC concert" => new BackstagePassStrategy(item),
                 "Sulfuras, Hand of Ragnaros" => new SulfurasStrategy(item),
+                { } x when x.StartsWith("Conjured") => new ConjuredItemStrategy(item),
                 _ => new NormalItemStrategy(item)
             };
         }
@@ -73,6 +74,34 @@ namespace GildedRoseKata
                     if (_item.Quality > 0)
                     {
                         _item.Quality = _item.Quality - 1;
+                    }
+                }
+            }
+        }
+
+        class ConjuredItemStrategy : ItemStrategyBase
+        {
+            private readonly Item _item;
+
+            public ConjuredItemStrategy(Item item)
+            {
+                _item = item;
+            }
+
+            public override void Update()
+            {
+                if (_item.Quality > 0)
+                {
+                    _item.Quality = _item.Quality - 2;
+                }
+
+                _item.SellIn = _item.SellIn - 1;
+
+                if (_item.SellIn < 0)
+                {
+                    if (_item.Quality > 0)
+                    {
+                        _item.Quality = _item.Quality - 2;
                     }
                 }
             }
