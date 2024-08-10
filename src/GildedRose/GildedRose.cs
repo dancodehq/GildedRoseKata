23dcd2e4b2660f6af7eb3525b9d773ac1069358e
Dan Harrison <harrison92d@gmail.com>
2024-08-10 10:45:55 +0100

    Duplicate UpdateBackstagePass method into BackstagePassStrategy class

--- src/GildedRose/GildedRose.cs
+++ src/GildedRose/GildedRose.cs
@@ -75,6 +75,50 @@ namespace GildedRoseKata
             }
         }
 
+        class BackstagePassStrategy
+        {
+            private readonly Item _item;
+
+            public BackstagePassStrategy(Item item)
+            {
+                _item = item;
+            }
+
+            public void Update()
+            {
+                if (_item.Quality < 50)
+                {
+                    _item.Quality = _item.Quality + 1;
+
+                    if (_item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (_item.SellIn < 11)
+                        {
+                            if (_item.Quality < 50)
+                            {
+                                _item.Quality = _item.Quality + 1;
+                            }
+                        }
+
+                        if (_item.SellIn < 6)
+                        {
+                            if (_item.Quality < 50)
+                            {
+                                _item.Quality = _item.Quality + 1;
+                            }
+                        }
+                    }
+                }
+
+                _item.SellIn = _item.SellIn - 1;
+
+                if (_item.SellIn < 0)
+                {
+                    _item.Quality = _item.Quality - _item.Quality;
+                }
+            }
+        }
+
         private void UpdateBackstagePass(Item item)
         {
             if (item.Quality < 50)
