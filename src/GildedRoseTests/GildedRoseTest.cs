d3be0eeb1a842fcd57e15c0a3f5bf2388110780f
Dan Harrison <harrison92d@gmail.com>
2024-08-13 16:04:34 +0100

    Consolidate tests

--- src/GildedRoseTests/GildedRoseTest.cs
+++ src/GildedRoseTests/GildedRoseTest.cs
@@ -10,7 +10,6 @@ namespace GildedRoseTests
         [InlineData("Sword", 5, 4)]
         [InlineData("Aged Brie", 5, 4)]
         [InlineData("Backstage passes to a TAFKAL80ETC concert", 5, 4)]
-        [InlineData("Sulfuras, Hand of Ragnaros", 5, 5)]
         [InlineData("Conjured Moon Cake", 5, 4)]
         public void Sell_in_is_updated_each_day(string itemName, int sellIn, int expectedSellIn)
         {
@@ -19,178 +18,72 @@ namespace GildedRoseTests
             app.UpdateQuality();
             Assert.Equal(expectedSellIn, items[0].SellIn);
         }
-        
-        [Fact]
-        public void Normal_item_quality_degrades_by_one_each_day_when_sell_by_is_in_the_future()
+
+        [Theory]
+        [InlineData(5, 10, 9)]
+        [InlineData(0, 10, 8)]
+        [InlineData(0, 1, 0)]
+        public void Normal_item_quality_is_updated_each_day(int sellIn, int quality, int expectedQuality)
         {
-            List<Item> items = [new Item { Name = "Sword", Quality = 10, SellIn = 5 }];
+            List<Item> items = [new Item { Name = "Sword", Quality = quality, SellIn = sellIn }];
             var app = new GildedRose(items);
             app.UpdateQuality();
-            Assert.Equal(9, items[0].Quality);
+            Assert.Equal(expectedQuality, items[0].Quality);
         }
-        
-        [Fact]
-        public void Normal_item_quality_degrades_by_two_each_day_when_sell_by_is_exceeded()
+
+        [Theory]
+        [InlineData(5, 10, 8)]
+        [InlineData(0, 10, 6)]
+        [InlineData(0, 3, 0)]
+        public void Conjured_item_quality_is_updated_each_day(int sellIn, int quality, int expectedQuality)
         {
-            List<Item> items = [new Item { Name = "Sword", Quality = 10, SellIn = 0 }];
+            List<Item> items = [new Item { Name = "Conjured Moon Cake", Quality = quality, SellIn = sellIn }];
             var app = new GildedRose(items);
             app.UpdateQuality();
-            Assert.Equal(8, items[0].Quality);
+            Assert.Equal(expectedQuality, items[0].Quality);
+        }
+
+        [Theory]
+        [InlineData(5, 10, 11)]
+        [InlineData(0, 10, 12)]
+        [InlineData(0, 49, 50)]
+        public void Aged_brie_item_quality_is_updated_each_day(int sellIn, int quality, int expectedQuality)
+        {
+            List<Item> items = [new Item { Name = "Aged Brie", Quality = quality, SellIn = sellIn }];
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(expectedQuality, items[0].Quality);
+        }
+
+        [Theory]
+        [InlineData(15, 10, 11)]
+        [InlineData(10, 10, 12)]
+        [InlineData(5, 10, 13)]
+        [InlineData(0, 10, 0)]
+        public void Backstage_pass_quality_is_updated_each_day(int sellIn, int quality, int expectedQuality)
+        {
+            List<Item> items = [new Item { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = quality, SellIn = sellIn }];
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(expectedQuality, items[0].Quality);
         }
 
         [Fact]
-        public void Normal_item_sell_by_decreases_by_one_each_day()
+        public void Sulfuras_quality_does_not_change()
         {
-            List<Item> items = [new Item { Name = "Sword", Quality = 10, SellIn = 5 }];
+            List<Item> items = [new Item { Name = "Sulfuras, Hand of Ragnaros", Quality = 80, SellIn = 0 }];
             var app = new GildedRose(items);
             app.UpdateQuality();
-            Assert.Equal(4, items[0].SellIn);
+            Assert.Equal(80, items[0].Quality);
         }
 
         [Fact]
-        public void Normal_item_quality_does_not_degrade_past_zero()
+        public void Sulfuras_sell_in_does_not_change()
         {
-            List<Item> items = [new Item { Name = "Sword", Quality = 0, SellIn = 0 }];
-            var app = new GildedRose(items);
-            app.UpdateQuality();
-            Assert.Equal(0, items[0].Quality);
-        }
-
-        [Fact]
-        public void Aged_brie_quality_improves_by_one_each_day_when_sell_by_is_in_the_future()
-        {
-            List<Item> items = [new Item { Name = "Aged Brie", Quality = 30, SellIn = 5 }];
-            var app = new GildedRose(items);
-            app.UpdateQuality();
-            Assert.Equal(31, items[0].Quality);
-        }
-        
-        [Fact]
-        public void Aged_brie_quality_improves_by_two_each_day_when_sell_by_is_exceeded()
-        {
-            List<Item> items = [new Item { Name = "Aged Brie", Quality = 30, SellIn = 0 }];
-            var app = new GildedRose(items);
-            app.UpdateQuality();
-            Assert.Equal(32, items[0].Quality);
-        }
-
-        [Fact]
-        public void Aged_brie_quality_never_improves_past_fifty()
-        {
-            List<Item> items = [new Item { Name = "Aged Brie", Quality = 50, SellIn = 0 }];
-            var app = new GildedRose(items);
-            app.UpdateQuality();
-            Assert.Equal(50, items[0].Quality);
-        }
-
-        [Fact]
-        public void Aged_brie_sell_by_decreases_by_one_each_day()
-        {
-            List<Item> items = [new Item { Name = "Aged Brie", Quality = 10, SellIn = 5 }];
-            var app = new GildedRose(items);
-            app.UpdateQuality();
-            Assert.Equal(4, items[0].SellIn);
-        }
-
-        [Theory, InlineData(0), InlineData(1)]
-        public void Sulfuras_quality_never_degrades(int sellIn)
-        {
-            List<Item> items = [new Item { Name = "Sulfuras, Hand of Ragnaros", Quality = 50, SellIn = sellIn }];
-            var app = new GildedRose(items);
-            app.UpdateQuality();
-            Assert.Equal(50, items[0].Quality);
-        }
-
-        [Fact]
-        public void Sulfuras_sell_by_never_decreases()
-        {
-            List<Item> items = [new Item { Name = "Sulfuras, Hand of Ragnaros", Quality = 50, SellIn = 10 }];
+            List<Item> items = [new Item { Name = "Sulfuras, Hand of Ragnaros", Quality = 80, SellIn = 10 }];
             var app = new GildedRose(items);
             app.UpdateQuality();
             Assert.Equal(10, items[0].SellIn);
         }
-
-        [Theory]
-        [InlineData(6)]
-        [InlineData(7)]
-        [InlineData(8)]
-        [InlineData(9)]
-        [InlineData(10)]
-        public void Backstage_pass_quality_improves_by_two_when_ten_days_or_less_remaining(int sellIn)
-        {
-            List<Item> items = [new Item { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 30, SellIn = sellIn }];
-            var app = new GildedRose(items);
-            app.UpdateQuality();
-            Assert.Equal(32, items[0].Quality);
-        }
-
-        [Theory]
-        [InlineData(1)]
-        [InlineData(2)]
-        [InlineData(3)]
-        [InlineData(4)]
-        [InlineData(5)]
-        public void Backstage_pass_quality_improves_by_three_when_five_days_or_less_remaining(int sellIn)
-        {
-            List<Item> items = [new Item { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 30, SellIn = sellIn }];
-            var app = new GildedRose(items);
-            app.UpdateQuality();
-            Assert.Equal(33, items[0].Quality);
-        }
-
-        [Fact]
-        public void Backstage_pass_quality_drops_to_zero_after_concert()
-        {
-            List<Item> items = [new Item { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 30, SellIn = 0 }];
-            var app = new GildedRose(items);
-            app.UpdateQuality();
-            Assert.Equal(0, items[0].Quality);
-        }
-
-        [Fact]
-        public void Backstage_pass_sell_by_decreases_by_one_each_day()
-        {
-            List<Item> items = [new Item { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 30, SellIn = 5 }];
-            var app = new GildedRose(items);
-            app.UpdateQuality();
-            Assert.Equal(4, items[0].SellIn);
-        }
-
-        [Fact]
-        public void Conjured_item_quality_degrades_by_two_each_day_when_sell_by_is_in_the_future()
-        {
-            List<Item> items = [new Item { Name = "Conjured Moon Cake", Quality = 30, SellIn = 5 }];
-            var app = new GildedRose(items);
-            app.UpdateQuality();
-            Assert.Equal(28, items[0].Quality);
-        }
-
-        [Fact]
-        public void Conjured_item_quality_degrades_by_four_each_day_when_sell_by_is_exceeded()
-        {
-            List<Item> items = [new Item { Name = "Conjured Moon Cake", Quality = 30, SellIn = 0 }];
-            var app = new GildedRose(items);
-            app.UpdateQuality();
-            Assert.Equal(26, items[0].Quality);
-        }
-        
-        [Fact]
-        public void Conjured_item_sell_by_decreases_by_one_each_day()
-        {
-            List<Item> items = [new Item { Name = "Conjured Moon Cake", Quality = 10, SellIn = 5 }];
-            var app = new GildedRose(items);
-            app.UpdateQuality();
-            Assert.Equal(4, items[0].SellIn);
-        }
-
-        [Fact]
-        public void Conjured_item_quality_does_not_degrade_past_zero()
-        {
-            List<Item> items = [new Item { Name = "Conjured Moon Cake", Quality = 3, SellIn = 0 }];
-            var app = new GildedRose(items);
-            app.UpdateQuality();
-            Assert.Equal(0, items[0].Quality);
-        }
-
     }
 }
